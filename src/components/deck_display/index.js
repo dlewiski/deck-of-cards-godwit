f6364e8e4bc03755858b214a406bb570e3bb9303
@lewidavi <lewidavi@umich.edu>
2021-12-08 15:14:50 -0700

    Added shuffle functionaly and display, going to deal cards to users

--- src/components/deck_display/index.js
+++ src/components/deck_display/index.js
@@ -1,8 +1,26 @@
+import {useState} from 'react'
 function DeckDisplay(props) {
+    const [currentDeck, setCurrentDeck] = useState([]);
+    const [numOfPlayers, setNumOfPlayers] = useState(1);
+
+    const shuffleDeck = () => {
+        console.log('shuffle deck is clicked')
+        let deckToShuffle = props.deck.map(card => card);
+        let deckLength = props.deck.length;
+        let randomPosition
+        while (deckLength) {
+            randomPosition = Math.floor(Math.random() * deckLength--);
+
+            [deckToShuffle[deckLength], deckToShuffle[randomPosition]] = [deckToShuffle[randomPosition], deckToShuffle[deckLength]];
+    }
+    console.log(deckToShuffle)
+    setCurrentDeck(deckToShuffle);
+    }
+
     return (
-      <div>
+      <div style={{maxWidth: '1200px'}}>
         <h1>List of cards</h1>
-        <div style={{display: 'flex'}}>
+        <div style={{display: 'flex', flexWrap: 'wrap'}}>
         {props.deck.map(card => (
             <div key={card.id} style={{border: '1px solid black', textAlign: 'center', margin: '4px 8px'}}>
                 <p>{card.suit}</p>
@@ -10,6 +28,20 @@ function DeckDisplay(props) {
             </div>
         ))}
         </div>
+        <h1>Shuffled deck</h1>
+        <button onClick={shuffleDeck}>Click to shuffle deck</button>
+        <div style={{display: 'flex', flexWrap: 'wrap'}}>
+            {currentDeck && (
+                currentDeck.map(card => (
+                    <div key={`${card.id}currentDeck`} style={{border: '1px solid black', textAlign: 'center', margin: '4px 8px'}}>
+                        <p>{card.suit}</p>
+                        <p>{card.cardValue}</p>
+                    </div>
+                ))
+            )}
+
+        </div>
+        <h1>Number of Players {numOfPlayers}</h1>
       </div>
     );
   }
